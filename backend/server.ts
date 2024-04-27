1c3ac514f3fe915bb791f121b352839a81ce3686
Esraa102 <manalellban9@gmailcom>
2024-04-27 03:26:51 +0300

    add middleware

--- backend/server.ts
+++ backend/server.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
 const app = express();
 
 const port = process.env.PORT || 5001;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    credentials: true,
+  })
+);
 
 app.listen(port, () => {
   console.log(`Server Is Running On ${port}`);
