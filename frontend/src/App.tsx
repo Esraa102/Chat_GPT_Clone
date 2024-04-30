97887e47f98ffc8fd5c5c4612ca5d01bc13ebdd9
Esraa102 <manalellban9@gmailcom>
2024-04-30 18:35:59 +0300

    create pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import { AuthLayout, Home, SignIn, SignUp } from "./pages";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <main>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route element={<AuthLayout />}>
+            <Route path="/sign-up" element={<SignUp />} />
+            <Route path="/sign-in" element={<SignIn />} />
+          </Route>
+        </Routes>
+      </div>
+    </main>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,5 @@
+import Home from "./Home";
+import AuthLayout from "./_auth/AuthLayout";
+import SignIn from "./_auth/SignIn";
+import SignUp from "./_auth/SignUp";
+export { Home, SignIn, SignUp, AuthLayout };
