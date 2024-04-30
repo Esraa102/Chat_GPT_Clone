35cad2885a0b5f79ef3b275f5b84f15988b7983a
Esraa102 <manalellban9@gmailcom>
2024-04-30 15:53:00 +0300

    add validation middleware

--- backend/routes/auth.route.ts
+++ backend/routes/auth.route.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import { registerUser } from "../controllers/auth.controller.js";
+import { registerValidator, validate } from "../utils/validators.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post("/register", validate(registerValidator), registerUser);
 export { router as authRouter };
--- dist/routes/auth.route.js
+++ dist/routes/auth.route.js
@@ -6,7 +6,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.authRouter = void 0;
 const express_1 = __importDefault(require("express"));
 const auth_controller_js_1 = require("../controllers/auth.controller.js");
+const validators_js_1 = require("../utils/validators.js");
 const router = express_1.default.Router();
 exports.authRouter = router;
-router.post("/register", auth_controller_js_1.registerUser);
+router.post("/register", (0, validators_js_1.validate)(validators_js_1.registerValidator), auth_controller_js_1.registerUser);
 //# sourceMappingURL=auth.route.js.map
